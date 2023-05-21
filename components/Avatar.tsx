966856e71726f211c5db34b9baaed6a010b79197
dubuduyo <mgmxmg93@naver.com>
2023-05-21 19:36:42 +0900

    design: User profile page components

---
+++ components/Avatar.tsx
@@ -0,0 +1,9 @@
+type AvatarSize = 'small' | 'medium' | 'large' | 'xlarge';
+
+export default function Avatar({ image }: { image?: string | null }) {
+  return (
+    <div className={getContainerStyle(size)}>
+      <img />
+    </div>
+  );
+}
---
+++ components/profile/UserProfile.tsx
@@ -0,0 +1,5 @@
+import styles from './UserProfile.module.css';
+
+export default function UserProfile() {
+  return <section className={styles.sectionContainer}></section>;
+}

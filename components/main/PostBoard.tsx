e8af7e78f9826b4ed6b1ee6656d3cc63b504e8c5
hoinlee-moi <lhi3048@gmail.com>
2023-05-22 18:24:22 +0900

    modify : 테스트 코드 주석처리

--- components/main/PostBoard.tsx
+++ components/main/PostBoard.tsx
@@ -5,16 +5,16 @@ import { getStorage, ref, getDownloadURL } from "@firebase/storage";
 import { useState } from "react";
 
 export default function PostBoard() {
-  const [imgUrl, setImgUrl] = useState("");
-  const storage = getStorage(app);
-  
-  getDownloadURL(ref(storage, "main_food_0.jpeg"))
-    .then((url) => {
-      setImgUrl(url);
-    })
-    .catch((error) => {
-      // Handle any errors
-    });
+//   const [imgUrl, setImgUrl] = useState("");
+//   const storage = getStorage(app);
+
+//   getDownloadURL(ref(storage, "main_food_0.jpeg"))
+//     .then((url) => {
+//       setImgUrl(url);
+//     })
+//     .catch((error) => {
+//       // Handle any errors
+//     });
 
   return (
     <div>

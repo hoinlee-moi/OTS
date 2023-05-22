63cae5d38513d55207ac06871b7eb12416a13c21
hoinlee-moi <lhi3048@gmail.com>
2023-05-22 17:35:29 +0900

    add 새로운 라이브러리 추가 / 몽고db , swr, firebase storage

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -1,7 +1,14 @@
-
-
 export default function Main() {
-    return <div>
-        <h2>안녕 메인</h2>
+  return (
+    <div>
+        <div>
+            <button>정렬</button>
+            <ul>
+                <li>최신순</li>
+                <li>좋아요순</li>
+            </ul>
+        </div>
+      <h2>안녕 메인</h2>
     </div>
+  );
 }

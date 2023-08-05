af4ccb3e807bd93967ac54f8872758794ad059a8
hoinlee-moi <lhi3048@gmail.com>
2023-08-06 03:28:14 +0900

    rename : image name change

--- components/home/ImageArray.tsx
+++ components/home/ImageArray.tsx
@@ -11,12 +11,12 @@ type imgArr = {
 export default function ImageArray({ props, order }: props) {
   const imgArr: imgArr = {
     a: [0, 1, 2, 0],
-    b: [3, 4, 10, 4],
-    c: [5, 6, 9, 6],
-    d: [1, 1, 10, 9],
+    b: [3, 4, 5, 4],
+    c: [7, 6, 9, 6],
+    d: [1, 2, 5, 9],
     e: [2, 8, 0, 8],
     f: [4, 9, 7, 9],
-    g: [5, 11, 1, 0],
+    g: [5, 0, 1, 0],
   };
   return (
     <div>
@@ -24,11 +24,12 @@ export default function ImageArray({ props, order }: props) {
         ? imgArr[props].map((v, idx) => {
             return (
               <Image
-                src={`/assets/main/main_food_${v}.jpeg`}
+                src={`/assets/main/main_food_${v}_v2.jpeg`}
                 alt="food"
                 key={idx}
-                width={255}
-                height={435}
+                width={500}
+                height={700}
+                quality={100}
                 priority
                 placeholder="blur"
                 blurDataURL="data:image/gif;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mO8fPl8PQAH7AL2dy8SSgAAAABJRU5ErkJggg=="
